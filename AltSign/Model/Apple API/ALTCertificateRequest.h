0e69b12f6bf25c6aa09401ef0b99210a3994297f
Joseph Mattello <mail@joemattiello.com>
2022-06-12 02:36:28 -0400

    fix nullable warnings
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>

--- AltSign/Model/Apple API/ALTCertificateRequest.h
+++ AltSign/Model/Apple API/ALTCertificateRequest.h
@@ -8,12 +8,10 @@
 
 #import <Foundation/Foundation.h>
 
-NS_ASSUME_NONNULL_BEGIN
-
 @interface ALTCertificateRequest : NSObject
 
-@property (nonatomic, copy, readonly) NSData *data;
-@property (nonatomic, copy, readonly) NSData *privateKey;
+@property (nonatomic, copy, readonly, nonnull) NSData *data;
+@property (nonatomic, copy, readonly, nonnull) NSData *privateKey;
 
 // Can't mark init nullable without warnings, so use factory method instead.
 + (nullable instancetype)newRequest NS_SWIFT_NAME(ALTCertificateRequest.makeRequest());
@@ -21,5 +19,3 @@ NS_ASSUME_NONNULL_BEGIN
 - (instancetype)init NS_UNAVAILABLE;
 
 @end
-
-NS_ASSUME_NONNULL_END
--- AltSign/Model/Apple API/ALTCertificateRequest.m
+++ AltSign/Model/Apple API/ALTCertificateRequest.m
@@ -17,12 +17,12 @@
     NSData *data = nil;
     NSData *privateKey = nil;
     [ALTCertificateRequest generateRequest:&data privateKey:&privateKey];
-    
+
     if (data == nil || privateKey == nil)
     {
         return nil;
     }
-    
+
     ALTCertificateRequest *request = [[ALTCertificateRequest alloc] initWithData:data privateKey:privateKey];
     return request;
 }
@@ -35,7 +35,7 @@
         _data = [data copy];
         _privateKey = [privateKey copy];
     }
-    
+
     return self;
 }
 
@@ -44,13 +44,13 @@
 {
     BIGNUM *bignum = NULL;
     __block RSA *rsa = NULL;
-    
+
     X509_REQ *request = NULL;
     EVP_PKEY *publicKey = NULL;
-    
+
     BIO *csr = NULL;
     BIO *privateKey = NULL;
-    
+
     void (^finish)(void) = ^{
         if (publicKey != NULL)
         {
@@ -61,45 +61,45 @@
         {
             RSA_free(rsa);
         }
-        
+
         BN_free(bignum);
         X509_REQ_free(request);
-        
+
         BIO_free_all(csr);
         BIO_free_all(privateKey);
     };
-    
+
     /* Generate RSA Key */
-    
+
     bignum = BN_new();
     if (BN_set_word(bignum, RSA_F4) != 1)
     {
         finish();
         return;
     }
-    
+
     rsa = RSA_new();
     if (RSA_generate_key_ex(rsa, 2048, bignum, NULL) != 1)
     {
         finish();
         return;
     }
-    
+
     /* Generate request */
-    
+
     const char *country = "US";
     const char *state = "CA";
     const char *city = "Los Angeles";
     const char *organization = "AltSign";
     const char *commonName = "AltSign";
-    
+
     request = X509_REQ_new();
     if (X509_REQ_set_version(request, 1) != 1)
     {
         finish();
         return;
     }
-    
+
     // Subject
     X509_NAME *subject = X509_REQ_get_subject_name(request);
     X509_NAME_add_entry_by_txt(subject, "C", MBSTRING_ASC, (const unsigned char *)country, -1, -1, 0);
@@ -107,24 +107,24 @@
     X509_NAME_add_entry_by_txt(subject, "L", MBSTRING_ASC, (const unsigned char*)city, -1, -1, 0);
     X509_NAME_add_entry_by_txt(subject, "O", MBSTRING_ASC, (const unsigned char*)organization, -1, -1, 0);
     X509_NAME_add_entry_by_txt(subject, "CN", MBSTRING_ASC, (const unsigned char*)commonName, -1, -1, 0);
-    
+
     // Public Key
     publicKey = EVP_PKEY_new();
     EVP_PKEY_assign_RSA(publicKey, rsa);
-    
+
     if (X509_REQ_set_pubkey(request, publicKey) != 1)
     {
         finish();
         return;
     }
-    
+
     // Sign request
     if (X509_REQ_sign(request, publicKey, EVP_sha1()) <= 0)
     {
         finish();
         return;
     }
-    
+
     // Output
     csr = BIO_new(BIO_s_mem());
     if (PEM_write_bio_X509_REQ(csr, request) != 1)
@@ -132,24 +132,24 @@
         finish();
         return;
     }
-    
+
     privateKey = BIO_new(BIO_s_mem());
     if (PEM_write_bio_RSAPrivateKey(privateKey, rsa, NULL, NULL, 0, NULL, NULL) != 1)
     {
         finish();
         return;
     }
-    
+
     /* Return values */
-    
+
     char *csrData = NULL;
     long csrLength = BIO_get_mem_data(csr, &csrData);
     *outputRequest = [NSData dataWithBytes:csrData length:csrLength];
-    
+
     char *privateKeyData = NULL;
     long privateKeyLength = BIO_get_mem_data(privateKey, &privateKeyData);
     *outputPrivateKey = [NSData dataWithBytes:privateKeyData length:privateKeyLength];
-    
+
     finish();
 }
 
