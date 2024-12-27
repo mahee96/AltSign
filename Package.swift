24c3210269d3f67c2392456fe50f8b9270284142
Magesh K <47920326+mahee96@users.noreply.github.com>
2024-12-27 21:17:02 +0530

    - prepare for rebase to altsign - 10 (updated Package.swift to be ready based on the refactoring)

--- Package.swift
+++ Package.swift
@@ -11,125 +11,118 @@ let package = Package(
         .macOS(.v11),
     ],
     products: [
-		// MARK: - AltSign
-        .library(
-            name: "AltSign",
-            targets: ["AltSign"]
-        ),
-
-        .library(
-            name: "AltSign-Static",
-            type: .static,
-            targets: ["AltSign"]
-        ),
-
         .library(
             name: "AltSign-Dynamic",
             type: .dynamic,
-            targets: ["AltSign"]
+            targets: ["AltSign", "CAltSign", "CoreCrypto", "CCoreCrypto", "ldid", "ldid-core", "OpenSSL"]
         ),
-
-		// MARK: - CoreCrypto
-		.library(
-			name: "CoreCrypto",
-			targets: ["CoreCrypto", "CCoreCrypto"]
-		),
-
-		.library(
-			name: "CoreCrypto-Static",
+        .library(
+            name: "AltSign-Static",
 			type: .static,
-			targets: ["CoreCrypto", "CCoreCrypto"]
-		),
-
-		.library(
-			name: "CoreCrypto-Dynamic",
-			type: .dynamic,
-			targets: ["CoreCrypto", "CCoreCrypto"]
-		),
-
-		// MARK: - CCoreCrypto
-		.library(
-			name: "CCoreCrypto",
-			targets: ["CCoreCrypto"]
-		),
-
-		.library(
-			name: "CCoreCrypto-Static",
-			type: .static,
-			targets: ["CCoreCrypto"]
-		),
-
-		.library(
-			name: "CCoreCrypto-Dynamic",
-			type: .dynamic,
-			targets: ["CCoreCrypto"]
-		)
+            targets: ["AltSign", "CAltSign", "CoreCrypto", "CCoreCrypto", "ldid", "ldid-core"]
+        )
     ],
+
+
+    //TODO: make targets use binaries as dependencies rather than compile from source
     dependencies: [
-        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", .upToNextMinor(from: "1.1.180")),
-		.package(url: "https://github.com/SideStore/iMobileDevice.swift", .upToNextMinor(from: "1.0.4"))
+//         .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", .upToNextMinor(from: "1.1.180")),
+//         .package(url: "https://github.com/SideStore/iMobileDevice.swift", .upToNextMinor(from: "1.0.4"))
     ],
+
+
+
+
+
     targets: [
-        // MARK: - AltSign
+        // exposing OpenSSL as target
+        .binaryTarget(
+            name: "OpenSSL",
+            path: "Dependencies/OpenSSL/Frameworks/OpenSSL.xcframework"
+        ),
 
         .target(
             name: "AltSign",
             dependencies: [
                 "CAltSign"
             ],
+            path: "AltSign/Sources",
             cSettings: [
-                .headerSearchPath("../minizip/include"),
-                .define("CORECRYPTO_DONOT_USE_TRANSPARENT_UNION=1")
+                .headerSearchPath("Dependencies/minizip"),
+                .define("CORECRYPTO_DONOT_USE_TRANSPARENT_UNION=1"),
             ]
+//             swiftSettings: [
+//                 .define("MARKETPLACE")
+//             ]
         ),
 
-		.testTarget(
-			name: "AltSignTests",
-			dependencies: ["AltSign"]
-		),
+//        .testTarget(
+//            name: "AltSignTests",
+//            dependencies: ["AltSign"]
+//        ),
+
 
         .target(
             name: "CAltSign",
             dependencies: [
                 "CoreCrypto",
                 "ldid",
-                "minizip"
+//                 "minizip"
             ],
-            publicHeadersPath: "include",
+            path: "",
+            exclude: [
+                "AltSign/ldid/alt_ldid.cpp",
+                "AltSign/ldid/alt_ldid.hpp",
+                "AltSign/Sources",
+                "AltSign/include/module.modulemap",
+                "Dependencies/corecrypto",
+                "Dependencies/ldid",
+                "Dependencies/OpenSSL",
+                "Dependencies/minizip/iowin32.c",
+                "Dependencies/minizip/Makefile",
+                "Dependencies/minizip/minizip.c",
+                "Dependencies/minizip/miniunz.c",
+            ],
+            publicHeadersPath: "AltSign/include",
             cSettings: [
-                .headerSearchPath("include/"),
-                .headerSearchPath("include/AltSign"),
-                .headerSearchPath("Capabilities"),
-                .headerSearchPath("../ldid"),
-                .headerSearchPath("../ldid/include"),
-                .headerSearchPath("../minizip/include"),
-                .headerSearchPath("../ldid"),
-                .define("unix", to: "1"),
+                // Recursive wildcard paths no longer work as of Xcode 16 :(
+                // .headerSearchPath("AltSign/**"),
+                .headerSearchPath("AltSign/include"),
+                .headerSearchPath("AltSign/include/AltSign"),
+                .headerSearchPath("AltSign/Capabilities"),
+                .headerSearchPath("Dependencies/ldid"),
+                .headerSearchPath("Dependencies/ldid/libplist/include"),
+                .headerSearchPath("Dependencies/minizip"),
+                .headerSearchPath("Dependencies/OpenSSL/ios/include"),
+                .define("unix=1"),
             ],
+            // multiple language targets are unsupported by xcode 16 ?!
             cxxSettings: [
-                .headerSearchPath("include/"),
-                .headerSearchPath("include/AltSign"),
-                .headerSearchPath("Capabilities"),
-                .headerSearchPath("../ldid"),
-                .headerSearchPath("../ldid/include"),
-                .headerSearchPath("../minizip/include"),
-                .headerSearchPath("../ldid"),
-                .define("unix", to: "1"),
+                .headerSearchPath("AltSign/include"),
+                .headerSearchPath("AltSign/include/AltSign"),
+                .headerSearchPath("AltSign/Capabilities"),
+                .headerSearchPath("Dependencies/ldid"),
+                .headerSearchPath("Dependencies/ldid/libplist/include"),
+                .headerSearchPath("Dependencies/minizip"),
+                .headerSearchPath("Dependencies/OpenSSL/ios/include"),
+             .define("unix=1"),
             ],
             linkerSettings: [
                 .linkedFramework("UIKit", .when(platforms: [.iOS])),
-                .linkedFramework("Security")
+                .linkedFramework("Security"),
             ]
         ),
 
-        // MARK: - ldid
+
+
 
         .target(
             name: "ldid-core",
-            dependencies: [
-                "OpenSSL",
-				.product(name: "libplist", package: "iMobileDevice.swift")
-            ],
+            // dependencies: [
+            //     "OpenSSL",
+            //     .product(name: "libplist", package: "iMobileDevice.swift")
+            // ],
+            path: "Dependencies/ldid",
             exclude: [
                 "ldid.hpp",
                 "ldid.cpp",
@@ -141,31 +134,77 @@ let package = Package(
                 "make.sh",
                 "deb.sh",
                 "plist.sh",
+
+                // if lib plist is included as dependency, then no need for source
+                "libplist/include",
+                "libplist/include/Makefile.am",
+                "libplist/fuzz",
+                "libplist/cython",
+                "libplist/m4",
+                "libplist/test",
+                "libplist/tools",
+                "libplist/AUTHORS",
+                "libplist/autogen.sh",
+                "libplist/configure.ac",
+                "libplist/COPYING",
+                "libplist/COPYING.LESSER",
+                "libplist/doxygen.cfg.in",
+                "libplist/Makefile.am",
+                "libplist/NEWS",
+                "libplist/README.md",
+                "libplist/src/Makefile.am",
+                "libplist/src/libplist++.pc.in",
+                "libplist/src/libplist.pc.in",
+                "libplist/libcnary/cnary.c",
+                "libplist/libcnary/COPYING",
+                "libplist/libcnary/Makefile.am",
+                "libplist/libcnary/README"
             ],
             sources: [
                 "lookup2.c",
+                "libplist/src",
+                "libplist/libcnary"
             ],
             publicHeadersPath: "",
             cSettings: [
+                .headerSearchPath("libplist/include"),
+                .headerSearchPath("libplist/src"),
+                .headerSearchPath("libplist/libcnary/include"),
+//                .headerSearchPath("../OpenSSL/ios/include"),
+
+                .unsafeFlags(["-w"])
+            ],
+            cxxSettings: [
+                .unsafeFlags(["-w"])
             ]
         ),
 
         .target(
             name: "ldid",
             dependencies: ["ldid-core"],
+            path: "AltSign/ldid",
+            exclude: [
+                "alt_ldid.hpp"
+            ],
             sources: [
                 "alt_ldid.cpp"
             ],
-            publicHeadersPath: "include",
+            publicHeadersPath: "",
             cSettings: [
-                .headerSearchPath("../ldid-core"),
+                .headerSearchPath("../../Dependencies/ldid"),
+                .headerSearchPath("../../Dependencies/ldid/libplist/include"),
+                .headerSearchPath("../../Dependencies/ldid/libplist/src"),
+                .headerSearchPath("../../Dependencies/ldid/libplist/libcnary/include"),
+//                .headerSearchPath("../../Dependencies/OpenSSL/ios/include"),
+            ],
+            cxxSettings: [
+                .unsafeFlags(["-w"])
             ]
         ),
 
-        // MARK: - CoreCrypto
-
         .target(
             name: "CCoreCrypto",
+            path: "Dependencies/corecrypto",
             exclude: [
                 "Sources/CoreCryptoMacros.swift"
             ],
@@ -178,8 +217,12 @@ let package = Package(
         .target(
             name: "CoreCrypto",
             dependencies: ["CCoreCrypto"],
+            path: "Dependencies/corecrypto/Sources",
             exclude: [
-                "Sources/ccsrp.m"
+                "ccsrp.m"
+            ],
+            sources: [
+                "CoreCryptoMacros.swift"
             ],
             cSettings: [
                 .define("CORECRYPTO_DONOT_USE_TRANSPARENT_UNION=1")
@@ -190,13 +233,14 @@ let package = Package(
 
         .target(
             name: "minizip",
+            path : "Dependencies/minizip"
             sources: [
                 "minizip/zip.c",
                 "minizip/unzip.c",
                 "minizip/ioapi.c"
             ],
-            publicHeadersPath: "include"
-		)
+            publicHeadersPath: ""
+        )
     ],
 
     cLanguageStandard: CLanguageStandard.gnu11,
